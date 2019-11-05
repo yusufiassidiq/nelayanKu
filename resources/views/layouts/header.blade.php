41b1fee3b258145f58e94a7dab7aa05a2d51a479
yusufiassidiq <yusufiassidiq@gmail.com>
2019-11-06 01:07:58 +0700

    register login ok

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -121,39 +121,23 @@
               <!-- The user image in the navbar-->
               <img src="AdminLTE-3.0.0/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
               <!-- hidden-xs hides the username on small devices so only the image appears. -->
-              <span class="hidden-xs">Alexander Pierce</span>
+              <span class="hidden-xs">{{ Auth::user()->name }}</span>
             </a>
             <ul class="dropdown-menu">
               <!-- The user image in the menu -->
               <li class="user-header">
                 <img src="AdminLTE-3.0.0/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-
-                <p>
-                  Alexander Pierce - Web Developer
-                 
-                </p>
-              </li>
-              <!-- Menu Body -->
-              <!-- <li class="user-body">
-                <div class="row"> -->
-                  <!-- <div class="col-xs-4 text-center">
-                    <a href="#">Followers</a>
-                  </div>
-                  <div class="col-xs-4 text-center">
-                    <a href="#">Sales</a>
-                  </div>
-                  <div class="col-xs-4 text-center">
-                    <a href="#">Friends</a>
-                  </div> -->
-                <!-- </div> -->
-                <!-- /.row -->
-                
+                <p>{{ Auth::user()->name }}</p>
               </li>
               <!-- Menu Footer-->
               <li class="user-footer" >
-                
                 <div id="wrapper">
-                  <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                    <a href="{{ route('logout') }}" class="btn btn-default btn-flat" 
+                    onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
                 </div>
               </li>
             </ul>

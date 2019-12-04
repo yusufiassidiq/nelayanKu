6e7cb66905b5a424ca2124bf72ac607c0275f483
yusufiassidiq <yusufiassidiq@gmail.com>
2019-12-05 00:55:47 +0700

    tambah requirement

--- resources/views/layouts/adminlte.blade.php
+++ resources/views/layouts/adminlte.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-  <title>AdminLTE 3 | Dashboard 3</title>
+  <title>Nelayanku</title>
 
   <!-- Font Awesome Icons -->
   <link rel="stylesheet" href="{{url('AdminLTE-3.0.0/plugins/fontawesome-free/css/all.min.css')}}">
